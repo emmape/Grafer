0b37f2da324417275e72ba99cea3c430596804a8
Emma Persson <emma.e.persson@me.com>
2015-02-23 16:18:04 +0100

    Skapat MyUndirectedGraph

---
+++ src/alda/graph/MyUndirctedGraph.java
@@ -0,0 +1,62 @@
+package alda.graph;
+
+import java.util.List;
+
+public class MyUndirctedGraph implements UndirectedGraph {
+
+	@Override
+	public int getNumberOfNodes() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumberOfEdges() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean add(Object newNode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean connect(Object node1, Object node2, int cost) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isConnected(Object node1, Object node2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getCost(Object node1, Object node2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List depthFirstSearch(Object start, Object end) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List breadthFirstSearch(Object start, Object end) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UndirectedGraph minimumSpanningTree() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}

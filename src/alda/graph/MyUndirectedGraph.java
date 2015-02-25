7f370cc1a57cff14fdd7db9be6713f387875daf9
Emma Persson <emma.e.persson@me.com>
2015-02-25 13:08:28 +0100

    getCost fungerar. dfs är fel....

--- src/alda/graph/MyUndirectedGraph.java
+++ src/alda/graph/MyUndirectedGraph.java
@@ -10,6 +10,8 @@ public class MyUndirectedGraph<T> implements UndirectedGraph<T> {
 
 	private Map<T, Map<T, Integer>> nodes = new HashMap<T, Map<T, Integer>>();
 	
+
+	
 	@Override
 	public int getNumberOfNodes() {
 		return nodes.size();
@@ -69,60 +71,59 @@ public class MyUndirectedGraph<T> implements UndirectedGraph<T> {
 
 	@Override
 	public int getCost(T node1, T node2) {
-		// TODO Dijkstra's algorithm
-		//return 0;
 		
-		List<T> path = depthFirstSearch(node1, node2);
-		
-		if(path.isEmpty()) return -1;
-		
-		int cost = 0;
-		
-		for (int i = 0; i < path.size() - (path.size() == 1 ? 0 : 1); i++) {
-			for (Entry<T, Integer> edge : nodes.get(path.get(i)).entrySet()) {
-				T next = i + 1 == path.size() ? path.get(i) : path.get(i + 1);
-				if(edge.getKey() == next){
-					cost += edge.getValue();
-					break;
+		int cost = -1;
+		if(isConnected(node1, node2)){
+			for (Entry<T, Integer> edge : nodes.get(node1).entrySet()) {
+				if(edge.getKey()==node2){
+					cost=edge.getValue();
 				}
 			}
 		}
-		
 		return cost;
 	}
 
 	@Override
 	public List<T> depthFirstSearch(T start, T end) {
+		ArrayList<T> visited=new ArrayList<T>();
+		if(start == end){
+			visited.add(start);
+			
+		}else {
+		dfs(start, end, visited);
+		}
+		
+		return visited;
+		
 		// TODO This might be solvable with recursion. EDIT: Done..? Can't test it without getCost().
 		//		Problem: circular path gives infinite loop. Solution: private method that has a 'visited' list that's passed in the args.
-		List<T> list = new ArrayList<T>();
-		
-		if(start == end){
-			list.add(start);
-			return list;
-		}
+
+	}
+	
+	private void dfs(T start, T end, ArrayList<T> visited){
 		
 		if(nodes.get(start) != null){
 			for (Entry<T, Integer> edge : nodes.get(start).entrySet()) {
-				T neighbour = edge.getKey();
+				if(!visited.contains(edge)){
+					T neighbour = edge.getKey();
 				
-				if(neighbour != start){		// Skip over looping edge.
-					list.addAll(depthFirstSearch(neighbour, end));
+				//if(neighbour != start){		// Skip over looping edge.
+					visited.add(neighbour);
+					dfs(neighbour, end, visited);
+					//visited.addAll(depthFirstSearch(neighbour, end));
 					
-					if(!list.isEmpty()){
-						list.add(start);
-						break;
-					}
+					//if(!visited.isEmpty()){
+						//visited.add(start);
+						//break;
+				//	}
 				}
 			}
 		}
-		
-		return list;
 	}
 
 	@Override
 	public List<T> breadthFirstSearch(T start, T end) {
-		// TODO This could be solved using a queue (kö).
+		// TODO This could be solved using a queue (kï¿½).
 		return null;
 	}
 
